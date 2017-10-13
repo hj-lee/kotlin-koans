094da9ba3f2f278df3175590bca7ca0bef84d3da
Hwijae Lee <Hwijae.Lee@soti.net>
2017-10-13 17:52:57 -0400

    25~31

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,17 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.time.LocalDate
+import java.util.*
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +19,29 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class MyDateIterator(val start: MyDate, val endInclusive: MyDate) : Iterator<MyDate> {
+    private var currentDate = start
+    override fun hasNext(): Boolean {
+        return currentDate <= endInclusive
+    }
+
+    override fun next(): MyDate {
+        val date = currentDate
+        currentDate = currentDate.nextDay()
+        return date
+    }
+
+}
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun iterator() = MyDateIterator(start, endInclusive)
+}
+
+
+operator fun MyDate.plus(interval: TimeInterval): MyDate = this.addTimeIntervals(interval, 1)
+
+class MulTimeInterval(val timeInterval: TimeInterval, val mul: Int)
+
+operator fun TimeInterval.times(i: Int) = MulTimeInterval(this, i)
+
+operator fun MyDate.plus(mulTimeInterval: MulTimeInterval): MyDate = this.addTimeIntervals(mulTimeInterval.timeInterval, mulTimeInterval.mul)
